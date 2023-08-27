e84f4418f6687287596b879ad0a3a1995886b8c7
PRONOY <pronoybanik82@gmail.com>
2023-08-27 20:51:50 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+
+app.get("/", (req, res) => {
+    res.send("Route is working! YaY!");
+});
+
+module.exports = app;
+
+
+
+
---
+++ server.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const app = require("./app");
+require("dotenv").config();
+require("colors");
+
+
+mongoose.connect(process.env.DATABASE).then(() => {
+    console.log('database connection is successful'.red);
+});
+
+// server port
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`App is running on port ${port}`.yellow.bold);
+});
