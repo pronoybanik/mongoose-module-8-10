c6b8fde38abc2e7e831ad14e03948dcdbb72ab0b
PRONOY <pronoybanik82@gmail.com>
2023-08-29 21:50:37 +0600

    create category route and controller

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(cors());
 // routes
 const productRoute = require('./routes/product.route');
 const brandRoute = require('./routes/brand.route');
+const categoryRoute = require('./routes/category.route');
 
 
 
@@ -20,6 +21,7 @@ app.get("/", (req, res) => {
 
 app.use('/api/v1/product', productRoute);
 app.use("/api/v1/brand", brandRoute);
+app.use("/api/v1/category", categoryRoute);
 
 
 module.exports = app;
