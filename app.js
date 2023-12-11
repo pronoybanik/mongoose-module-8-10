78156ab61584452e558fd07d2f2127954226bc31
PRONOY <pronoybanik82@gmail.com>
2023-12-11 19:46:10 +0600

    change file change

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(cors());
 // routes
 const productRoute = require('./routes/product.route');
 const brandRoute = require('./routes/brand.route');
-const categoryRoute = require('./routes/category.route');
+// const categoryRoute = require('./routes/category.route');
 const supplierRoute = require('./routes/supplier.route');
 const stockRoute = require('./routes/stock.route');
 const storeRoute = require('./routes/store.route');
@@ -24,7 +24,7 @@ const paymentRoute = require('./routes/payment.route');
 
 app.use('/api/v1/product', productRoute);
 app.use("/api/v1/brand", brandRoute);
-app.use("/api/v1/category", categoryRoute);
+// app.use("/api/v1/category", categoryRoute);
 app.use("/api/v1/supplier", supplierRoute);
 app.use("/api/v1/stock", stockRoute);
 app.use("/api/v1/store", storeRoute);
--- routes/product.route.js
+++ routes/product.route.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const productController = require('../controllers/product.controller')
-const fileUploader = require('../middleware/fileUploader');
 const authorization = require('../middleware/authorization');
 const verifyToken = require('../middleware/verifyToken');
 
 
-router.post("/file-upload", fileUploader.single("image"), productController.fileUpload)
+// router.post("/file-upload", fileUploader.single("image"), productController.fileUpload)
 // router.post("/file-upload", fileUploader.array("image"), productController.fileUpload)
 
 router.route('/')
